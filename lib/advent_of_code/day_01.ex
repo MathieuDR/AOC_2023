d737401480d998d93d6fc1e8fdd49bcf30754b22
Mathieu De Raedt <mathieu@deraedt.dev>
2023-12-02 17:06:00 +0100

    Day 1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,44 @@
 defmodule AdventOfCode.Day01 do
-  def part1(_args) do
+  def part1(args) do
+    solve(args)
   end
 
-  def part2(_args) do
+  def part2(args) do
+    solve(args)
+  end
+
+  def solve(input) do
+    input
+    |> clean_input()
+    |> Enum.map(&calculate_line/1)
+    |> Enum.sum()
+  end
+
+  def calculate_line(line) do
+    numbers =
+      line
+      |> parse()
+
+    List.first(numbers) * 10 + List.last(numbers)
+  end
+
+  @numbers ~w[one two three four five six seven eight nine]
+
+  def parse(""), do: []
+  def parse(<<i>> <> rest) when i in ?1..?9, do: [i - ?0 | parse(rest)]
+
+  for {word, i} <- Enum.zip(@numbers, 1..9) do
+    def parse(unquote(word) <> _ = line) do
+      <<_>> <> rest = line
+      [unquote(i) | parse(rest)]
+    end
+  end
+
+  def parse(<<_>> <> rest), do: parse(rest)
+
+  defp clean_input(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
   end
 end
--- test/advent_of_code/day_01_test.exs
+++ test/advent_of_code/day_01_test.exs
@@ -3,19 +3,42 @@ defmodule AdventOfCode.Day01Test do
 
   import AdventOfCode.Day01
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = """
+    1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet
+    """
+
     result = part1(input)
 
-    assert result
+    assert result == 142
   end
 
-  @tag :skip
+  test "special_case" do
+    input = "1oneight"
+    assert part2(input) == 18
+  end
+
+  test "parse one" do
+    input = "abasonepijf"
+    assert part2(input) == 11
+  end
+
   test "part2" do
-    input = nil
+    input = """
+    two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen
+    """
+
     result = part2(input)
 
-    assert result
+    assert result == 281
   end
 end
